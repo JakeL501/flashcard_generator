8f7f65b8f25354629800944e0fafa0c2eb8f48eb
JakeL501 <jacob.letourneau@my.colby-sawyer.edu>
2017-12-04 18:02:34 -0500

    files and api sources

--- basic_card.js
+++ basic_card.js
@@ -1,1 +1,35 @@
+//use fs npm package to read and write to text files
+var fs = require("fs");
+
+// define constructor that will store and retrieve data for basic flashcards
+var BasicCard =function(front,back){
+	
+	  	this.front = front;
+	  	this.back = back;
+	  	
+	  	this.storeCard = function (front,back){
+			// this.cardFronts.push(front);
+			// this.cardBacks.push(back);
+			if(!front){
+			console.log("Front text was not entered")
+			}
+			if(!back){
+			console.log("Back text was not entered")
+			}
+			else{
+	        fs.appendFile("log.txt", "," + "front:" + front +","+ "Back:" + back);
+			}
+		}
+
+		this.cardFront = function (front){
+	        fs.appendFile("frontlog.txt", "," + front);
+		}
+		
+		this.cardBack = function (back){
+	        fs.appendFile("backlog.txt", "," + back);
+		}
+}
+
+// make constructor available so it can be called from another js file
+
 module.exports = BasicCard;
--- cloze_card.js
+++ cloze_card.js
@@ -1,1 +1,31 @@
+//use fs npm package to read and write to text files
+var fs = require("fs");
+
+// define constructor that will store and retrieve data for cloze flashcards
+var ClozeCard=function(text,cloze){
+		this.text = text;
+		this.cloze= cloze;
+	
+		this.fullText = function (text,cloze){
+			if(!cloze){
+				console.log("Deletion text was not entered")
+			}
+			if(!text){
+				console.log("Partial text was not entered")
+			}
+			else{
+		        fs.appendFile("fulllog.txt", "," + text +" " + cloze);
+			}
+		}
+		
+	this.partialText = function (text){
+	        fs.appendFile("partiallog.txt", "," + text);
+		}
+	this.deletion = function (cloze){
+	        fs.appendFile("clozelog.txt", "," + cloze);
+		}
+}
+
+// make constructor available so it can be called from another js file
+
 module.exports = ClozeCard;
